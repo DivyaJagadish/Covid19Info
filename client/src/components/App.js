ebce8ff69c4079e13e85cc597c2abb14414abb6a
Divya Jagadish <divyajagadish21@gmail.com>
2021-06-08 08:22:10 +0000

    adds map

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,25 +1,30 @@
 import React from "react";
-
-import SimpleGlobe from "./globe.js";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "../App.css";
+import "leaflet/dist/leaflet.css";
 
 function App() {
 	const [data, setData] = React.useState(null);
 
-	React.useEffect(() => {
-		fetch("/api")
-			.then((res) => res.json())
-			.then((data) => setData(data.message));
-	}, []);
+	// React.useEffect(() => {
+	// 	fetch("/api")
+	// 		.then((res) => res.json())
+	// 		.then((data) => setData(data.message));
+	// }, []);
 
 	return (
 		<>
-			<SimpleGlobe />
-			<div className="App">
-				<header className="App-header">
-					<p>{!data ? "Loading..." : data}</p>
-				</header>
-			</div>
+			<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+				<TileLayer
+					attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+				/>
+				<Marker position={[51.505, -0.09]}>
+					<Popup>
+						A pretty CSS3 popup. <br /> Easily customizable.
+					</Popup>
+				</Marker>
+			</MapContainer>
 		</>
 	);
 }
