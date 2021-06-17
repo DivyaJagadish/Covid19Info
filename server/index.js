f1b89799fd0983b0fbb66a60409a52837ccbe475
Divya Jagadish <divyajagadish21@gmail.com>
2021-06-17 22:30:01 +0000

    herokku deployment

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 const geoRev = require("geo-reverse");
-
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/api/lat=:lat&&long=:long", (req, res) => {
@@ -28,4 +30,9 @@ app.get("/api/lat=:lat&&long=:long", (req, res) => {
 
 app.listen(PORT, () => {
 	console.log(`Example app listening on port ${PORT}`);
+});
+
+// All other GET requests not handled before will return our React app
+app.get("*", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
